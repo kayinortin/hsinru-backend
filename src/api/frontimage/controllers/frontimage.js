309d1d4f981188dee7253deb749557f84087857a
Edison <kayinortin@hotmail.com>
2023-12-20 21:38:56 +0800

    init

---
+++ src/api/frontimage/controllers/frontimage.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * frontimage controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::frontimage.frontimage');
---
+++ src/api/frontimage/routes/frontimage.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * frontimage router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::frontimage.frontimage');
